5c6b7f517179729f7499ebf9526f4f8b5aa92d5b
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-04-10 17:27:46 +0200

    feat: creation of infinite countdown.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-// TEST
-alert("ciao")
-
 // Simulare un countdown di 10 secondi che alla fine dice "Buon anno!"
 // Iniziate stampando il countdown nella console e dopo stampatelo in pagina.
+
+// Creo una variabile per i secondi del countdown (sono stati inseriti 11 sec per visualizzare concretamente i 10 secondi all' avvio della pagina)
+let seconds = 11;
+
+// Creo il clock invertito che esegue la funzione ad intervalli di 1 sec
+const clock = setInterval(function () {
+    // Se i secondi sono minori o uguali allora
+    if (seconds <= 11) {
+        //decrementa il valore dei secondi 
+        seconds--;
+    }
+    // Log alla console
+    console.log(seconds);
+    // inseriamo il valore anche in pagina
+    document.getElementById("second").innerText = seconds;
+}, 1000);
+
+
+
+
